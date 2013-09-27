821bc3f8901cf302a8259733a4876287e561788e
Jean Boussier <jean.boussier@gmail.com>
2013-09-27 14:43:10 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BawsaqHistory::Application.config.secret_key_base = '2bef89a7af3afc030a19fa7bffd832621964c016c561e30fe3452e58242cf84166ed554d839ad9cfae192f3373bd827342180674a12cf1546256e3060961784d'
