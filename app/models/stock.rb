5fd2372db7e64aaeb1f54b2b24fa20b60d2d2ec6
Jean Boussier <jean.boussier@gmail.com>
2013-09-29 13:31:01 -0400

    Add relative price

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -10,7 +10,8 @@ class Stock < ActiveRecord::Base
       @previous = previous
     end
 
-    delegate :price, to: :price_point
+    delegate :price, :stock, to: :price_point
+    delegate :lower_price, :higher_price, to: :stock, prefix: true
 
     def growth
       return 0 unless previous
@@ -18,11 +19,21 @@ class Stock < ActiveRecord::Base
       diff / previous.price * 100.0
     end
 
+    def relative
+      return 100 if price_point.price == stock_higher_price
+      return 0 if price_point.price == stock_lower_price
+
+      normalized_high = stock_higher_price - stock_lower_price
+      normalized_current = price_point.price - stock_lower_price
+      normalized_current / normalized_high * 100.0
+    end
+
     def as_json
       {
         at: price_point.created_at,
         price: price,
         growth: growth,
+        relative: relative
       }
     end
 
@@ -37,11 +48,11 @@ class Stock < ActiveRecord::Base
   end
 
   def lower_price
-    price_points.map(&:price).min
+    @lower_price ||= price_points.map(&:price).min
   end
 
   def higher_price
-    price_points.map(&:price).max
+    @higher_price ||= price_points.map(&:price).max
   end
 
 end
